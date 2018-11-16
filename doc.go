9e7952e2bab1274c6591cbbcaed594ed75a4ecbd
Oliver Eilhard <oliver@eilhard.net>
2018-11-16 14:03:22 +0100

    Fix typo

--- doc.go
+++ doc.go
@@ -1,5 +1,5 @@
 /*
-Package nuallable simplifies working with null values in Go.
+Package nullable simplifies working with null values in Go.
 
 You can use it to return e.g. the zero value of a variable in case
 it is a nil pointer.
